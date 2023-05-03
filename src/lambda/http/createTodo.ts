359f8178fa4f51df56d3ebdfc760bd74b88348d2
trungphan <trungphan>
2023-05-03 10:08:37 +0700

    first commit

---
+++ src/lambda/http/createTodo.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils';
+import { createTodo } from '../../businessLogic/todos'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+    // TODO: Implement creating a new TODO item
+    const authorization = event.headers.Authorization
+    const split = authorization.split(' ')
+    const jwtToken = split[1]
+    const item = await createTodo(newTodo, jwtToken)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        item: item
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
